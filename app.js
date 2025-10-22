be5855f97634a1c2871b6ee424707b904272dd8b
A-bacate <giovanna.c.magagnin@gmail.com>
2025-10-22 08:51:46 -0300

    settings

---
+++ app.js
@@ -0,0 +1,7 @@
+const express = require('');
+const mysql = require('');
+const cors = require('');
+const path = require('');
+const bcrypt = require('');
+const crypto = require('');
+const config = require('');
